f7c508130575c5311b490d61ca4267ab871e59fb
BukkitGerman <43503309+BukkitGerman@users.noreply.github.com>
2022-02-10 09:47:09 +0100

    make printBootScreen private

--- src/ServiceProvider/BootServiceProvider.ts
+++ src/ServiceProvider/BootServiceProvider.ts
@@ -1,16 +1,11 @@
-import "prompt";
 import { readdir } from "fs";
 export class BootServiceProvider {
     constructor(){
         this.printBootScreen();
-        prompt.start();
-        prompt.get(['username'], (res:object) => {
-            console.log(res)
-        })
     }
 
 
-    printBootScreen = () => {
+    private printBootScreen = () => {
         console.log("#######################################################################\n")
         console.log("-> Author: \tBukkitGerman\t/\tJustin Preuß \n-> Github: \thttps://github.com/BukkitGerman\n-> Repository: \thttps://github.com/BukkitGerman/g4l-twitch-bot\n");
         console.log("#######################################################################\n")
